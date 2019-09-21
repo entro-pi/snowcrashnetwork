dbd374f3c19d8ef3b671408901eb5cdbb56f6901
Taylor Belland <taylorbelland@gmail.com>
2019-09-20 21:04:33 -0700

    lookin good

--- menus.js
+++ menus.js
@@ -8,24 +8,34 @@ var values = {
 	cypher: " <br>Cypher",
 	chatlog: " <br>Chatlog",
 	crash: " <br>Crash",
-  content: " <br>Content",
+  inventory: " <br>inventory",
 };
 
-function RemoveContent() {
-        $('.content').css('display', 'none');
-        $('.content').css('animation', '.5s 1 twirl');
-        $('.content').css('webkitAnimationName', 'twirl');
+function RemoveInventory() {
+        $('.inventory').css('display', 'none');
+        $('.inventory').css('animation', '.5s 1 twirl');
+        $('.inventory').css('webkitAnimationName', 'twirl');
+        console.log("WEBKIDEND");
+}
+
+function RemoveMap() {
+        $('.mapContent').css('display', 'none');
+        $('.mapContent').css('animation', '.5s 1 slideopen');
+        $('.mapContent').css('webkitAnimationName', 'slideopen');
         console.log("WEBKIDEND");
 }
 
 var characterShown = false;
+var mapShown = false;
 $(document).ready(function() {
 
+
+  //for all items in the menu
   $('.item').mouseup(function () {
     if (characterShown) {
-     $('.content').css('webkitAnimationName', 'sink');
-      $('.content').css('animation', '.25s 1 sink');
-      document.addEventListener('webkitAnimationEnd', RemoveContent, true);
+     $('.inventory').css('webkitAnimationName', 'sink');
+      $('.inventory').css('animation', '.25s 1 sink');
+      document.addEventListener('webkitAnimationEnd', RemoveInventory, true);
       console.log(characterShown);
       characterShown = false;
       return;
@@ -33,19 +43,52 @@ $(document).ready(function() {
 
   });
 
+  //Map
+  $('#map').mouseup(function() {
+    if (mapShown) {
+     $('.mapContent').css('webkitAnimationName', 'sink');
+      $('.mapContent').css('animation', '.25s 1 sink');
+      document.addEventListener('webkitAnimationEnd', RemoveMap, true);
+      mapShown = false;
+      console.log(characterShown);
+      return;
+    };
+    if (!mapShown) {
+      $('.mapContent').css('display', 'block');
+
+      document.removeEventListener('webkitAnimationEnd', RemoveMap, true);
+      mapShown = true;
+      console.log(characterShown);
+    };
+  if (characterShown) {
+     $('.inventory').css('webkitAnimationName', 'sink');
+      $('.inventory').css('animation', '.25s 1 sink');
+      document.addEventListener('webkitAnimationEnd', RemoveInventory, true);
+      console.log(characterShown);
+      characterShown = false;
+      return;
+    };
+
+
+  });
+
+
+
+
+  //Character
   $('#character').mouseup(function() {
     if (characterShown) {
-     $('.content').css('webkitAnimationName', 'sink');
-      $('.content').css('animation', '.25s 1 sink');
-      document.addEventListener('webkitAnimationEnd', RemoveContent, true);
+     $('.inventory').css('webkitAnimationName', 'sink');
+      $('.inventory').css('animation', '.25s 1 sink');
+      document.addEventListener('webkitAnimationEnd', RemoveInventory, true);
       characterShown = false;
       console.log(characterShown);
       return;
     };
     if (!characterShown) {
-      $('.content').css('display', 'grid');
+      $('.inventory').css('display', 'grid');
 
-      document.removeEventListener('webkitAnimationEnd', RemoveContent, true);
+      document.removeEventListener('webkitAnimationEnd', RemoveInventory, true);
       characterShown = true;
       console.log(characterShown);
     };
